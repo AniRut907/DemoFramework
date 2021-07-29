dbaf2a8a7117c61326e1e265c7c2e5788d5a9e15
Aniket Lokhande <Aniket.Lokhande@extentia.com>
2021-07-29 16:31:22 +0530

    integrated files

--- DemoFramework/DemoFramework/Manager.swift
+++ DemoFramework/DemoFramework/Manager.swift
@@ -13,7 +13,7 @@ public class Manager {
     public func createManager(){
         print("Manager created successfully!")
         print("Thank you so much")
-        print("Can i go a head")
+        print("git hub integration completed")
     }
     public func isValidEmail(_ email: String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
