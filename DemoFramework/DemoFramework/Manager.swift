d2280b460463383cc8f60149d1fe4d618f737a00
Aniket Lokhande <Aniket.Lokhande@extentia.com>
2021-07-30 09:33:25 +0530

    changes in manger file

--- DemoFramework/DemoFramework/Manager.swift
+++ DemoFramework/DemoFramework/Manager.swift
@@ -15,15 +15,14 @@ public class Manager {
         print("Thank you so much")
         print("git hub integration completed")
         
-        let str = """
+        print("""
              git clone
              change directory
              git add . (before doing any commit or any new file add)
              git commit -m "message for commit"
              git push origin branchName
              
-        """
-        print(str)
+        """)
     }
     public func isValidEmail(_ email: String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
