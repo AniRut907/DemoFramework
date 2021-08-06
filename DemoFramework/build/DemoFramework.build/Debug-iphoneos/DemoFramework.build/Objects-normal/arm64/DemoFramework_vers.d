677091a7eb83f3778f582ddfa7458551cd2a028b
Aniket Lokhande <Aniket.Lokhande@extentia.com>
2021-08-06 17:02:19 +0530

    removed demoapp

--- DemoFramework/build/DemoFramework.build/Debug-iphoneos/DemoFramework.build/Objects-normal/arm64/DemoFramework_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/aniket.lokhande/Documents/Practice/Framework/DemoFramework/DemoFramework/build/DemoFramework.build/Debug-iphoneos/DemoFramework.build/DerivedSources/DemoFramework_vers.c
--- DemoFramework/build/DemoFramework.build/Debug-iphonesimulator/DemoFramework.build/Objects-normal/x86_64/DemoFramework_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/aniket.lokhande/Documents/Practice/Framework/DemoFramework/DemoFramework/build/DemoFramework.build/Debug-iphonesimulator/DemoFramework.build/DerivedSources/DemoFramework_vers.c
